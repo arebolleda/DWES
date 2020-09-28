01c30820f3e66be926417f93b3817f2d8086832c
RodrigoCanoM <71818543+RodrigoCanoM@users.noreply.github.com>
2020-09-28 10:42:53 +0200

    Create ej1Calculadora.php

---
+++ ej1Calculadora.php
@@ -0,0 +1,17 @@
+<HTML>
+<HEAD> <TITLE> Calculadora </TITLE> </HEAD>
+<BODY>
+<h1>Calculadora</h1>
+<?php
+$n1=53;
+$n2=4;
+echo "$n1 es del tipo". gettype($n1)."<br/>";
+echo "$n2 es del tipo". gettype($n2)."<br/>";
+echo $n1 ." + " . $n2. "= ".($n1+$n2). "<br>";
+print $n1 ." - " . $n2. "= ".($n1-$n2). "<br>" ;
+print $n1 ." * " . $n2. "= ".($n1*$n2). "<br>" ;
+echo $n1 ." / " . $n2. "= ".($n1/$n2). "<br>" ;
+echo $n1 ."++ = " .($n1++);
+?>
+</BODY>
+</HTML>
