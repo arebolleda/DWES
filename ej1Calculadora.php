7e1f3fcd0ece787c4119207c24aac80c741a6acd
RodrigoCanoM <71818543+RodrigoCanoM@users.noreply.github.com>
2020-09-28 10:44:45 +0200

    Update ej1Calculadora.php

--- ej1Calculadora.php
+++ ej1Calculadora.php
@@ -4,7 +4,7 @@
 <h1>Calculadora</h1>
 <?php
 $n1=53;
-$n2=4;
+$n2=4.2;
 echo "$n1 es del tipo". gettype($n1)."<br/>";
 echo "$n2 es del tipo". gettype($n2)."<br/>";
 echo $n1 ." + " . $n2. "= ".($n1+$n2). "<br>";
