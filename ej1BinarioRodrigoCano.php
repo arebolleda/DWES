771bc4bde1873d9697a2ad49968d4cc694950397
RodrigoCanoM <71818543+RodrigoCanoM@users.noreply.github.com>
2020-09-29 13:54:20 +0200

    Create ej1BinarioRodrigoCano.php

---
+++ ej1BinarioRodrigoCano.php
@@ -0,0 +1,24 @@
+<HTML>
+<HEAD><TITLE> EJ1-Conversion IP Decimal a Binario de Rodrigo Cano </TITLE>
+</HEAD>
+<BODY>
+<?php
+$ip="192.18.16.204";
+echo $ip;
+$pun=strpos($ip, ".");
+printf (" en binario es %b.",substr($ip,0,$pun));
+
+$ip2=substr($ip, ++$pun);
+$pun2=strpos($ip2, ".");
+printf ("%b.",substr($ip2,0,$pun2));
+
+$ip3=substr($ip2, ++$pun2);
+$pun3=strpos($ip3, ".");
+printf ("%b.",substr($ip3,0,$pun3));
+
+$ip4=substr($ip3, ++$pun3);
+printf ("%b\n",$ip4);
+
+?>
+</BODY>
+</HTML>
