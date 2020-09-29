bd68dce4733764baa444ed2085a5b1b92d46972e
RodrigoCanoM <71818543+RodrigoCanoM@users.noreply.github.com>
2020-09-29 14:00:35 +0200

    Create ej3BinarioRodrigoCano.php

---
+++ ej3BinarioRodrigoCano.php
@@ -0,0 +1,28 @@
+<HTML>
+<HEAD><TITLE> EJ1-Conversion IP Decimal a Binario </TITLE></HEAD>
+<BODY>
+<?php
+$ip="192.168.16.100/16";
+echo $ip ."=";
+$pun=strpos($ip, ".");
+#printf (" en binario es %b.",substr($ip,0,$pun));
+$ip1= substr($ip,0,$pun);
+
+echo str_pad(decbin($ip1),8,"0",STR_PAD_LEFT);
+
+$ip2=substr($ip, ++$pun);
+$pun2=strpos($ip2, ".");
+#printf ("%b.",substr($ip2,0,$pun2));
+echo ".".str_pad(decbin($ip2),8,"0",STR_PAD_LEFT);
+
+$ip3=substr($ip2, ++$pun2);
+$pun3=strpos($ip3, ".");
+#printf ("%b.",substr($ip3,0,$pun3));
+echo ".". str_pad(decbin($ip3),8,"0",STR_PAD_LEFT);
+
+$ip4=substr($ip3, ++$pun3);
+#printf ("%b\n",$ip4);
+echo ".". str_pad(decbin($ip4),8,"0",STR_PAD_LEFT);;
+?>
+</BODY>
+</HTML>
