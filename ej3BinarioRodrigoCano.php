fb6dabbc778cf92cb2d4866633077076f2343935
RodrigoCanoM <71818543+RodrigoCanoM@users.noreply.github.com>
2020-10-01 13:56:56 +0200

    Update ej3BinarioRodrigoCano.php

--- ej3BinarioRodrigoCano.php
+++ ej3BinarioRodrigoCano.php
@@ -2,13 +2,11 @@
 <HEAD><TITLE> EJ1-Conversion IP Decimal a Binario </TITLE></HEAD>
 <BODY>
 <?php
-$ip="192.168.16.100/16";
+$ip="192.165.16.100/16";
 $pun=strpos($ip, "/");
 $mask= substr($ip,++$pun);
 echo "IP :".$ip."</br>";
 echo "Mascara: ".$mask."</br>";
-
-echo "Direccion Red: ";
 
 //echo $ip ."=";
 $pun=strpos($ip, ".");
@@ -37,13 +35,17 @@ $pun=strpos($ip1, "/");
 $ip2= substr($ip1,0,$pun);//100
 $mask4=""+str_pad(decbin($ip2),8,"0",STR_PAD_LEFT);//100 en bin
 //echo str_pad(decbin($ip2),8,"0",STR_PAD_LEFT)."<br>";
-//$ip1= substr($ip2,++$pun);// mascara:16
+echo "<br>".$ip1."d-d".$ip2."<br>";
+$ip1= substr($ip1,++$pun);// mascara:16
 echo $mask1.".".$mask2.".".$mask3.".".$mask4;
 echo "<br>";
+echo $ip1."-".$ip2;
 //$mask=$mask1."".$mask2."".$mask3."".$mask4;
-$mask= str_pad(substr($mask1."".$mask2."".$mask3."".$mask4,0,16),32,"0",STR_PAD_RIGHT);
+$mask= str_pad(substr($mask1."".$mask2."".$mask3."".$mask4,0,$ip1),32,"0",STR_PAD_RIGHT); //mask.0.0
 echo "<br>";
-echo substr($mask,0,8).".".substr($mask,8,16).".".substr($mask,16,24).".".substr($mask,24,32);
+echo substr($mask,0,8).".".substr($mask,8,8).".".substr($mask,16,8).".".substr($mask,24)."<br>";
+echo "Direccion Red: ".bindec(substr($mask,0,8)).".".bindec(substr($mask,8,8)).".".bindec(substr($mask,16,8)).".".bindec(substr($mask,24));
+$mask=$mask
 ?>
 </BODY>
 </HTML>
