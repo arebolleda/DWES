d0da410ef3b235b5fcc6058648bb3ad77cc6064e
RodrigoCanoM <71818543+RodrigoCanoM@users.noreply.github.com>
2020-10-01 09:11:26 +0200

    Update ej1BinarioRodrigoCano.php

--- ej1BinarioRodrigoCano.php
+++ ej1BinarioRodrigoCano.php
@@ -6,18 +6,18 @@
 $ip="192.18.16.204";
 echo $ip;
 $pun=strpos($ip, ".");
-printf (" en binario es %b.",substr($ip,0,$pun));
+printf (" en binario es %08b.",substr($ip,0,$pun));
 
 $ip2=substr($ip, ++$pun);
 $pun2=strpos($ip2, ".");
-printf ("%b.",substr($ip2,0,$pun2));
+printf ("%08b.",substr($ip2,0,$pun2));
 
 $ip3=substr($ip2, ++$pun2);
 $pun3=strpos($ip3, ".");
-printf ("%b.",substr($ip3,0,$pun3));
+printf ("%08b.",substr($ip3,0,$pun3));
 
 $ip4=substr($ip3, ++$pun3);
-printf ("%b\n",$ip4);
+printf ("%08b\n",$ip4);
 
 ?>
 </BODY>
