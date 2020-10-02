22a61d427cb2b5aaa0816bc3002da05f1a40cacc
RodrigoCanoM <71818543+RodrigoCanoM@users.noreply.github.com>
2020-10-02 11:38:29 +0200

    Update ej3BinarioRodrigoCano.php

--- ej3BinarioRodrigoCano.php
+++ ej3BinarioRodrigoCano.php
@@ -2,7 +2,7 @@
 <HEAD><TITLE> EJ1-Conversion IP Decimal a Binario </TITLE></HEAD>
 <BODY>
 <?php
-$ip="192.165.16.100/16";
+$ip="10.33.15.100/8";
 $pun=strpos($ip, "/");
 $mask= substr($ip,++$pun);
 echo "IP :".$ip."</br>";
@@ -35,17 +35,23 @@ $pun=strpos($ip1, "/");
 $ip2= substr($ip1,0,$pun);//100
 $mask4=""+str_pad(decbin($ip2),8,"0",STR_PAD_LEFT);//100 en bin
 //echo str_pad(decbin($ip2),8,"0",STR_PAD_LEFT)."<br>";
-echo "<br>".$ip1."d-d".$ip2."<br>";
+//echo "<br>".$ip1."d-d".$ip2."<br>";
 $ip1= substr($ip1,++$pun);// mascara:16
-echo $mask1.".".$mask2.".".$mask3.".".$mask4;
-echo "<br>";
-echo $ip1."-".$ip2;
+//echo $mask1.".".$mask2.".".$mask3.".".$mask4;
+//echo "<br>";
+//echo $ip1."-".$ip2;
 //$mask=$mask1."".$mask2."".$mask3."".$mask4;
 $mask= str_pad(substr($mask1."".$mask2."".$mask3."".$mask4,0,$ip1),32,"0",STR_PAD_RIGHT); //mask.0.0
-echo "<br>";
-echo substr($mask,0,8).".".substr($mask,8,8).".".substr($mask,16,8).".".substr($mask,24)."<br>";
+//echo "<br>";
+//echo substr($mask,0,8).".".substr($mask,8,8).".".substr($mask,16,8).".".substr($mask,24)."<br>";
 echo "Direccion Red: ".bindec(substr($mask,0,8)).".".bindec(substr($mask,8,8)).".".bindec(substr($mask,16,8)).".".bindec(substr($mask,24));
-$mask=$mask
+$broadCast=str_pad(substr($mask1."".$mask2."".$mask3."".$mask4,0,$ip1),32,"1",STR_PAD_RIGHT);
+//echo "<br>".$broadCast."<br>";//Mask.255.255
+echo "<br>";
+echo "Direccion Broadcast: ".bindec(substr($broadCast,0,8)).".".bindec(substr($broadCast,8,8)).".".bindec(substr($broadCast,16,8)).".".bindec(substr($broadCast,24));
+echo "<br>";
+echo "Rango: ".bindec(substr($mask,0,8)).".".bindec(substr($mask,8,8)).".".bindec(substr($mask,16,8)).".".((bindec(substr($mask,24)))+1);
+echo " a ".bindec(substr($broadCast,0,8)).".".bindec(substr($broadCast,8,8)).".".bindec(substr($broadCast,16,8)).".".((bindec(substr($broadCast,24)))-1);
 ?>
 </BODY>
 </HTML>
